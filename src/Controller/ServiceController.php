422b8480672dcc8d6527fc97cc844e7c12f46943
davidjeje <38698702+davidjeje@users.noreply.github.com>
2021-01-27 02:41:31 +0100

    Update ServiceController.php

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Service;
 use App\Form\ServiceType;
 use App\Repository\ServiceRepository;
+use App\Repository\ImageRepository;
 use App\Entity\Paginator;
 use App\Form\PaginatorType;
 use App\Repository\PaginatorRepository;
@@ -19,53 +20,54 @@ class ServiceController extends AbstractController
     /**
      * @Route("/", name="service_index", methods={"GET"})
      */
-    public function index(ServiceRepository $serviceRepository): Response
+    public function index(ServiceRepository $serviceRepository, ImageRepository $imageRepository): Response
     {
+        
         return $this->render('service/index.html.twig', [
-            'services' => $serviceRepository->findAll(),
+            'services' => $serviceRepository->findAll(), 'picture' => $imageRepository->findOneBySomeField(1)
         ]);
     }
 
     /**
      * @Route("/about", name="service_about", methods={"GET"})
      */
-    public function about(): Response
+    public function about(ImageRepository $imageRepository): Response
     {
-        return $this->render('service/about.html.twig');
+        return $this->render('service/about.html.twig', ['picture' => $imageRepository->findOneBySomeField(1)]);
     }
 
     /**
      * @Route("/login", name="login_or_registration", methods={"GET"})
      */
-    public function loginOrRegistration(): Response
+    public function loginOrRegistration(ImageRepository $imageRepository): Response
     {
-        return $this->render('service/loginOrRegistration.html.twig');
+        return $this->render('service/loginOrRegistration.html.twig', ['picture' => $imageRepository->findOneBySomeField(1)]);
     }
 
     /**
      * @Route("/service/{page}", name="service_service", methods={"GET"})
      */
-    public function service(ServiceRepository $serviceRepository, $page): Response
+    public function service(ServiceRepository $serviceRepository, $page, ImageRepository $imageRepository): Response
     {
         $nombreMaxParPage = 6;
         $nombreMax = 6;
         $firstResult = ($page-1) * $nombreMaxParPage;
         //Cette variable stock la fonction située dans le repository commentaire et qui elle même stock des valeurs pour lui permettre d'afficher un nombre de commentaire par figure et par page.
-        $nombreService = $serviceRepository->nombreService($firstResult, $nombreMax);
+        $serviceNumber = $serviceRepository->serviceNumber($firstResult, $nombreMax);
 
         //Cette variable stock la fonction située dans le repository paginator et qui elle même stock des valeurs pour lui permettre d'afficher un nombre de commentaire par figure et par page.
-        $findAllPaginator = $serviceRepository->findAllPaginator($page, $nombreMaxParPage);
+        $findAllPage = $serviceRepository->findAllPage($page, $nombreMaxParPage);
         
         $pagination = array(
             'page' => $page,
-            'nbPages' => ceil(count($findAllPaginator) / $nombreMaxParPage),
+            'nbPages' => ceil(count($findAllPage) / $nombreMaxParPage),
             'nomRoute' => 'service_service',
             'paramsRoute' => array()
         );
-
         return $this->render('service/service.html.twig', [
-            /*'services' => $serviceRepository->findAll(),*/ 'nombreService' => $nombreService,'pagination' => $pagination
+            /*'services' => $serviceRepository->findAll(),*/'serviceNumber' => $serviceNumber, 'pagination' => $pagination, 'picture' => $imageRepository->findOneBySomeField(1)
         ]);
+
     }
 
     #[Route('/new', name: 'service_new', methods: ['GET', 'POST'])]
@@ -90,12 +92,12 @@ class ServiceController extends AbstractController
     } 
 
     /**
-     * @Route("/{id}", name="service_show", methods={"GET"})
+     * @Route("/details/{id}", name="service_show", methods={"GET"})
      */
-    public function show(Service $service): Response
+    public function show(Service $service, ImageRepository $imageRepository): Response
     {
         return $this->render('service/show.html.twig', [
-            'service' => $service,
+            'service' => $service, 'picture' => $imageRepository->findOneBySomeField(1)
         ]);
     }
 
